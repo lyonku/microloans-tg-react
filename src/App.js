23a679ba103474fa1bc9a5fa0c5227781f7ea8a8
lyonku <z.pilipenko123@gmail.com>
2024-08-01 18:47:00 +0300

    add design

--- src/App.js
+++ src/App.js
@@ -7,7 +7,56 @@ function App() {
     tg.ready();
   }, []);
 
-  return <div className="App">апвапв</div>;
+  const microLoansMass = [
+    { img: "/imgs/zaimer.png", link: "" },
+    { img: "/imgs/zaimer.png", link: "" },
+    { img: "/imgs/zaimer.png", link: "" },
+    { img: "/imgs/zaimer.png", link: "" },
+  ];
+
+  return (
+    <div className="App">
+      <header className="header">
+        <img src="/imgs/SmartMoneyLogo.png" />
+        <h2>Умные деньги</h2>
+      </header>
+      <main className="main">
+        <h1 className="main__title">Доступные предложения</h1>
+        <ul className="microloans__list">
+          {microLoansMass.map((microloan, index) => {
+            return (
+              <li className="microloans__item" key={index}>
+                <div className="microloans__item-header">
+                  <img
+                    className="microloans__item-img"
+                    src="/imgs/zaimer.png"
+                  />
+                  <div className="microloans__item_chance">
+                    <div className="microloans__item_chance-wrap">
+                      <p className="microloans__item_chance-title">Высокий</p>
+                      <p className="microloans__item_chance-text">
+                        шанс одобрения
+                      </p>
+                    </div>
+                    <img
+                      className="microloans__item_chance-img"
+                      src="/imgs/highChance.svg"
+                    />
+                  </div>
+                </div>
+                <a
+                  href={microloan.link}
+                  className="SolidBtn microloans__item-btn"
+                >
+                  Получить займ
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default App;
